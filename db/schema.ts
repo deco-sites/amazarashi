a192d332ed569c91f70c595d220140a71be6a96b
Guilherme da Silva Benevides <git@gui.dev.br>
2024-10-06 20:42:49 -0300

    feat: add initial tables in database

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,6 @@
-
 /**
-* The code snippet below is an example.
-*/
+ * The code snippet below is an example.
+ */
 
 /**
 * import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
@@ -12,3 +11,21 @@ export const profiles = sqliteTable("profiles", {
   email: text("email"),
 });
 */
+
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const albuns = sqliteTable("albuns", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  image: text("image").notNull(),
+});
+
+export const tracks = sqliteTable("tracks", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  duration: text("duration").notNull(),
+  album_id: text("album_id")
+    .notNull()
+    .references(() => albuns.id),
+});
